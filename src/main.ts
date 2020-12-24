01dbcb89dafc14f88fd4ac4783f8080bf7ff0124
Russkih Anatoly <titanofdarkness@gmail.com>
2020-12-24 13:28:58 +1000

    Minor add swagger initial.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Link Shortener API')
+    .setDescription('The Link Shortener API')
+    .setVersion('1.0')
+    .addTag('auth')
+    .addTag('link-engine')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
   Logger.log(`App will run on: http://localhost:${port}`);
   await app.listen(port);
