022b4758084a729d539170fb88a08794008b8d73
Russkih Anatoly <titanofdarkness@gmail.com>
2020-12-24 10:37:37 +1000

    Minor tune.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  Logger.log(`App will run on: http://localhost:${port}`);
+  await app.listen(port);
 }
 bootstrap();
