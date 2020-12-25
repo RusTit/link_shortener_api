39c531d0a332e4bdc7e75e3bf89f1cb70767ded0
Russkih Anatoly <titanofdarkness@gmail.com>
2020-12-25 18:48:40 +1000

    Fix test.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,13 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { JweService } from './jweService';
+import parseJwk from 'jose/jwk/parse';
+
+export const mockPublicKey = async () => {
+  return await parseJwk(
+    {
+      e: 'AQAB',
+      n:
+        'qpzYkTGRKSUcd12hZaJnYEKVLfdEsqu6HBAxZgRSvzLFj_zTSAEXjbf3fX47MPEHRw8NDcEXPjVOz84t4FTXYF2w2_LGWfp_myjV8pR6oUUncJjS7DhnUmTG5bpuK2HFXRMRJYz_iNR48xRJPMoY84jrnhdIFx8Tqv6w4ZHVyEvcvloPgwG3UjLidP6jmqbTiJtidVLnpQJRuFNFQJiluQXBZ1nOLC7raQshu7L9y0IatVU7vf0BPnmuSkcNNvmQkSta6ODQBPaL5-o5SW8H37vQjPDkrlJpreViNa3jqP5DB5HYUO-DMh4FegRv9gZWLDEvXpSd9A13YXCa9Q8K_w',
+      kty: 'RSA',
+    },
+    'RSA-OAEP-256',
+  );
+};
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService, JweService],
+      providers: [
+        AuthService,
+        JweService,
+        {
+          provide: 'PUBLIC_ASYMMETRIC_KEY',
+          useFactory: mockPublicKey,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
