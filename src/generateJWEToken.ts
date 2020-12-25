5a14d24fb53de16d8a2c835228cc0b3762cff5e5
Russkih Anatoly <titanofdarkness@gmail.com>
2020-12-25 15:21:25 +1000

    Minor.

--- src/generateJWEToken.ts
+++ src/generateJWEToken.ts
@@ -8,18 +8,39 @@ export async function savePlain(fileName: string, key: JWK): Promise<void> {
   await fs.writeFile(fileName, dataStr);
 }
 
+export async function fileExist(fileName: string): Promise<boolean> {
+  try {
+    await fs.access(fileName);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
 export async function generateSymmetricKey(): Promise<void> {
+  const fileName = 'secretKey.json';
+  if (await fileExist(fileName)) {
+    return;
+  }
   const secretKey = await generateSecret('HS256');
   const secretKeyPlain = await fromKeyLike(secretKey);
-  await savePlain('secretKey.json', secretKeyPlain);
+  await savePlain(fileName, secretKeyPlain);
 }
 
 export async function generateAsymmetricKeys(): Promise<void> {
+  const privateKeyFileName = 'privateKey.json';
+  const publicKeyFileName = 'publicKey.json';
+  if (
+    (await fileExist(publicKeyFileName)) &&
+    (await fileExist(privateKeyFileName))
+  ) {
+    return;
+  }
   const { publicKey, privateKey } = await generateKeyPair('PS256');
   const publicKeyPlain = await fromKeyLike(publicKey);
-  await savePlain('publicKey.json', publicKeyPlain);
+  await savePlain(publicKeyFileName, publicKeyPlain);
   const privateKeyPlain = await fromKeyLike(privateKey);
-  await savePlain('privateKey.json', privateKeyPlain);
+  await savePlain(privateKeyFileName, privateKeyPlain);
 }
 
 export async function run(): Promise<void> {
