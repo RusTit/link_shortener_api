ab10332b2dc81b353f40ce75a25f2e49ad5a2283
Russkih Anatoly <titanofdarkness@gmail.com>
2020-12-26 19:35:16 +1000

    Fix for processors extension.

--- src/schedule-tasks/schedule-tasks.module.ts
+++ src/schedule-tasks/schedule-tasks.module.ts
@@ -5,11 +5,14 @@ import { ReportPreAggregationService } from './report-pre-aggregation.service';
 import { BullModule } from '@nestjs/bull';
 import { join } from 'path';
 
+const pos = __filename.lastIndexOf('.');
+const ext = __filename.slice(pos + 1);
+
 @Module({
   imports: [
     BullModule.registerQueue({
       name: 'report',
-      processors: [join(__dirname, 'jobs', 'report-pre-aggregation.js')],
+      processors: [join(__dirname, 'jobs', `report-pre-aggregation.${ext}`)],
     }),
   ],
   providers: [
